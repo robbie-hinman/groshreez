b066ac01698f8836a3129b09b0f3a6d0cdd65d17
Robbie Hinman <robbie.hinman@gmail.com>
2019-05-01 21:50:09 -0700

    added actionTypes

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,5 @@
+export const ADD_ITEM = 'ADD_ITEM';
+export const TOGGLE_ITEM = 'TOGGLE_ITEM';
+export const REMOVE_ITEM = 'REMOVE_ITEM';
+export const SET_VISIBLE_FILTER = 'SET_VISIBLE_FILTER';
+export const TOGGLE_ADD_MODAL = 'TOGGLE_ADD_MODAL';
---
+++ src/actions/groceryList.js
@@ -0,0 +1,3 @@
+import * as actionTypes from './actionTypes';
+
+export const toggleAddModal = () => ({ type: actionTypes.TOGGLE_ADD_MODAL });
